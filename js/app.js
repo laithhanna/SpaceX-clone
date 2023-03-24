e4f3a34f64756d6fd33540d38d86f55a36a28f1a
Laith <laithhanna6@gmail.com>
2023-03-23 21:28:01 -0400

    Added dark overlay when hamburger icon is clicked

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
 
 btn.addEventListener("click", (e) => {
   btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
 });
